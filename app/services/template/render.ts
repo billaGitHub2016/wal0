0697e483773dd0674ad01a8afa20537c1745290d
billa <billa2013@163.com>
2025-05-20 21:38:03 +0800

    fix: setup env params

--- app/services/template/render.ts
+++ app/services/template/render.ts
@@ -1,17 +1,18 @@
-import pug from 'pug';
-import fs from 'fs';
-import path from 'path';
+import pug from "pug"
+import fs from "fs"
+import path from "path"
 
 export function renderPreviewPage(componentData: any) {
   // 读取模板文件
   const templateContent = fs.readFileSync(
-    path.join(process.cwd(), 'public', 'templates', 'test.pug'),
-    'utf-8'
-  );
-  
+    path.join(process.cwd(), "public", "templates", "test.pug"),
+    "utf-8",
+  )
+
   // 编译并渲染模板
-  const compiledFunction = pug.compile(templateContent);
+  const compiledFunction = pug.compile(templateContent)
   return compiledFunction({
-    componentData
-  });
+    componentData,
+    renderUrl: process.env.SHADCN_RENDER || "http://localhost:3001",
+  })
 }
