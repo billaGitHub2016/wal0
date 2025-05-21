469c7f45f4a5ebe02e9c8299b1664bbaaa7532be
billa <billa2013@163.com>
2025-05-21 23:08:50 +0800

    build: build test

--- lib/env.ts
+++ lib/env.ts
@@ -13,7 +13,7 @@ export const env = createEnv({
     NEXT_GITHUB_ID: z.string().min(1),
     NEXT_GITHUB_SECRET: z.string().min(1),
     WALLET_KEY: z.string().min(1),
-    SITE_BUILDER: z.string() || z.undefined(),
+    SITE_BUILDER: z.string(),
   },
   client: {
     NEXT_PUBLIC_PACKAGE_ID: z.string().min(1),
